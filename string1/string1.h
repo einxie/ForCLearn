3694bbdd4e3b54f803634d650b9dd1fdbf7b80fb
Ein Xie <einxie@gmail.com>
2013-04-28 00:02:10 +0800

    class of string in 20130427

---
+++ string1/string1.h
@@ -0,0 +1,36 @@
+#ifndef STRING1_H
+#define STRING1_H
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class String {
+private:
+    char* str;
+    int len;
+    static int num_strings;
+    static const int CINLIM = 80;
+public:
+    String(void);
+    String(const char* s);
+    String(const String& s);
+    ~String(void);
+    int length(void) const {
+        return len;
+    }
+
+    String& operator= (const String& s);
+    String& operator= (const char* s);
+    char& operator[] (int i);
+    const char& operator[] (int i) const;
+    
+    friend bool operator< (const String& st1, const String& st2);
+    friend bool operator> (const String& st1, const String& st2);
+    friend bool operator== (const String& st1, const String& st2);
+    friend ostream& operator<< (ostream& os, const String& st);
+    friend istream& operator>> (istream& is, String& st);
+
+    static int howMany(void);
+};
+
+#endif
